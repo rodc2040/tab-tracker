06ff55bb89ba373972ae06d6c79b0d925af50740
Jerrard <rodc2040@Jerrards-MBP.attlocal.net>
2018-10-14 20:11:30 -0400

    setting up the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="register">
+   <h1>Register</h1>
+   <div>
+    <input
+        v-bind="email"
+        type="email"
+        name="email"
+        placeholder="Enter your email">
+  </div>
+
+  <div>
+    <input
+      v-bind="password"
+      type="password"
+      name="password"
+      placeholder="Enter your password">
+    </div>
+    <div>
+    <button
+    @click="register">
+    Register
+    </button>
+  </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationServices from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationServices.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
